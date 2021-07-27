4ed98eaa26e199984c720f7858fb94210c7f91c9
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-07-27 11:50:49 -0500

    Added completed display. Adding adjustments with minor styling

--- js/search-app.js
+++ js/search-app.js
@@ -26,9 +26,18 @@ function handleResponse(data) {
     let results = data.results;
     let html = '';
     results.forEach(function (result) {
-        html += '<div class="shadow-lg p-3 mb-5 bg-white rounded" style="height: 32rem; width: 28rem;">';
+        html += '<div class="card card-div p-3 mb-5 bg-white" style="height: 30rem; width: 20rem;">';
         html += '<div class="card-body flex-column d-flex justify-content-center">';
-        html += '<img class="card-img thumbnail mx-auto d-block" src=" ' + result.imageUrl + '"' + '/>';
+        html += '<img class="card-img mx-auto" src=" ' + result.imageUrl + '"' + '/>';
+        html += '<div class="col">';
+        html += '<p>' + result.name + '</p>';
+        html += '<div class="row">';
+        if (result.msrp !== null && result.msrp > result.price) {
+            html += '<p>' + result.msrp + '</p>';
+        }
+        html += '<p>' + result.price + '</p>';
+        html += '</div>';
+        html += '</div>';
         html += '</div>';
         html += '</div>';
     })
